d7b94b1a5b4b3caa1103fb22602ab4c63a1eae8b
Kayce <pat.nzediegwu@gmail.com>
2024-07-15 22:57:10 -0700

    feat: add list accounts api route

--- api/server.go
+++ api/server.go
@@ -20,6 +20,7 @@ func NewServer(store *db.Store) *Server {
 	// routes
 	router.POST("/accounts", server.createAccount)
 	router.GET("/accounts/:id", server.getAccount)
+	router.GET("/accounts", server.listAccounts)
 
 	server.router = router
 	return server
