39eeb7128eb0676332164797bfc9c99d9d7f02ea
Kayce <pat.nzediegwu@gmail.com>
2024-07-17 17:06:21 -0700

    feat: add user model

--- db/sqlc/models.go
+++ db/sqlc/models.go
@@ -35,3 +35,13 @@ type Transfer struct {
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 }
+
+type User struct {
+	Username          string    `json:"username"`
+	HashedPassword    string    `json:"hashed_password"`
+	Fullname          string    `json:"fullname"`
+	Email             string    `json:"email"`
+	PasswordChangedAt time.Time `json:"password_changed_at"`
+	CreatedAt         time.Time `json:"created_at"`
+	UpdatedAt         time.Time `json:"updated_at"`
+}
